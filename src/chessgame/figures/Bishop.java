ef17c503223c1b2b81ec6ded7e19f9e01ee1d7f0
VoKi21 <vovakilchenko@gmail.com>
2023-12-20 19:12:53 +0300

    Bishop returns up to 6 move sequences, or attacking moves, if it's not empty

--- src/chessgame/figures/Bishop.java
+++ src/chessgame/figures/Bishop.java
@@ -1,6 +1,11 @@
 package chessgame.figures;
 
+import chessgame.Field;
+import chessgame.Moves;
+import chessgame.util.BrittleList;
+import chessgame.util.Directions;
 import chessgame.util.Position;
+import com.sun.jdi.connect.AttachingConnector;
 
 public class Bishop extends Figure {
     public Bishop(Position position, String color) {
@@ -10,5 +15,65 @@ public class Bishop extends Figure {
     @Override
     public Figures getType() {
         return Figures.BISHOP;
+    }
+
+    @Override
+    public Moves getMoves(Field field) {
+        Moves toReturn = new Moves();
+        Moves attackingMoves = new Moves();
+        for (int i = 0; i < 6; i++) {
+            BrittleList<Position> sequenceToAdd = new BrittleList<>();
+
+            Position positionToAdd = this.position;
+            for (int j = 0; j < 5; j++) {
+                switch (i / 2) {
+                    case 0 -> {
+                        positionToAdd = positionToAdd
+                                .move(Directions.LEFTABOVE)
+                                .move(i % 2 == 0 ? Directions.LEFTBELOW  : Directions.ABOVE);
+                    }
+                    case 1 -> {
+                        positionToAdd = positionToAdd
+                                .move(Directions.RIGHTABOVE)
+                                .move(i % 2 == 0 ? Directions.ABOVE      : Directions.RIGHTBELOW);
+                    }
+                    case 2 -> {
+                        positionToAdd = positionToAdd
+                                .move(Directions.BELOW)
+                                .move(i % 2 == 0 ? Directions.RIGHTBELOW : Directions.LEFTBELOW);
+                    }
+                }
+                if (positionToAdd.getX() < 0) {
+                    break;
+                }
+                sequenceToAdd.add(positionToAdd);
+            }
+
+            for (int j = 0; j < sequenceToAdd.size(); j++) {
+                Position positionInSequence = sequenceToAdd.get(j);
+                Figure figureOnPosition = field.getFigureOnPosition(positionInSequence);
+                if (figureOnPosition.getType() == Figures.FREE) {
+                    continue;
+                }
+                if (figureOnPosition.getColor() == this.color) {
+                    sequenceToAdd.remove(j);
+                    break;
+                } else {
+                    attackingMoves.add(positionInSequence);
+                    if (j + 1 < sequenceToAdd.size()) {
+                        sequenceToAdd.remove(j + 1);
+                    }
+                }
+            }
+
+            if (!sequenceToAdd.isEmpty()) {
+                toReturn.add(sequenceToAdd);
+            }
+        }
+        if (attackingMoves.getMovesToOnePoint().isEmpty()) {
+            return toReturn;
+        } else {
+            return attackingMoves;
+        }
     }
 }
