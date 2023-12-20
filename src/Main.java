923c3e8f14442048167bd9da65e0102a44d91e06
VoKi21 <vovakilchenko@gmail.com>
2023-12-20 22:29:25 +0300

    Game in while loop, without sleep() or timer

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,11 @@ import chessgame.graphics.FieldPrinter;
 public class Main {
     public static void main(String[] args) {
         Field field = new Field();
+        boolean result = true;
+        while (result) {
+            FieldPrinter.printField(field);
+            result = field.step();
+        }
         FieldPrinter.printField(field);
     }
 }
